3e18cc3d159e1dbcc5345c9b37445519694f8299
Henrique Curi <henriquecuridemiranda@hotmail.com>
2023-03-23 05:10:21 -0300

    first version

--- mac_changer.py
+++ mac_changer.py
@@ -2,9 +2,18 @@
 
 import subprocess
 
+print("---- MAC Changer ----\n\n")
+
+
 interface=str(input("Interface: ")).strip()
-newMac=str(input("New Mac (keep it blank for random): ")).strip()
+
+print("\n\n- Keep it blank to a random one")
+print("- Type 'd' for the original\n\n")
+
+newMac=str(input("New Mac: ")).strip()
 
 subprocess.call(f"ifconfig {interface} down", shell=True)
 subprocess.call(f"ifconfig {interface} hw ether {newMac}", shell=True)
 subprocess.call(f"ifconfig {interface} up", shell=True)
+
+print(f"[+] {interface} MAC Adress changed to {newMac}")
