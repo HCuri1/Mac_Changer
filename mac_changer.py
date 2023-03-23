66c5678d20c05397acb06470c273620472b3ebee
Henrique Curi <henriquecuridemiranda@hotmail.com>
2023-03-23 05:04:17 -0300

    first version

--- mac_changer.py
+++ mac_changer.py
@@ -1,3 +1,10 @@
 # /usr/bin/env python
 
 import subprocess
+
+interface=str(input("Interface: ")).strip()
+newMac=str(input("New Mac (keep it blank for random): ")).strip()
+
+subprocess.call(f"ifconfig {interface} down", shell=True)
+subprocess.call(f"ifconfig {interface} hw ether {newMac}", shell=True)
+subprocess.call(f"ifconfig {interface} up", shell=True)
