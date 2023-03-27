e36289e1fa93ff77fe8fb64cd2edb1c2c4e669aa
HCuri1 <henriquecuridemiranda@hotmail.com>
2023-03-27 04:02:42 -0300

    fix bugs

--- random_change.py
+++ random_change.py
@@ -3,4 +3,4 @@ from subprocess import call
 def randomChange(interface):
     
     call(["./random_changer.sh", interface])
-    print(f"\n[+] {interface} MAC Adress changed!")
+    print(f"\n[+] {interface} MAC Address changed!")
